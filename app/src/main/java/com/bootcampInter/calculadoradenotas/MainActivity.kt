abed742ae0dc5e72be36ff368d0915f66943e3e6
Marcelo Assis <marceloassis50@gmail.com>
2021-07-18 16:46:49 -0300

    declaração de variaveis, clique do botão e logica de resultado, criacao no strings.xml

--- app/src/main/java/com/bootcampInter/calculadoradenotas/MainActivity.kt
+++ app/src/main/java/com/bootcampInter/calculadoradenotas/MainActivity.kt
@@ -2,10 +2,32 @@ package com.bootcampInter.calculadoradenotas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 //este é o main
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btCalcular = buttonCalcular
+        val resultado = textViewResultado
+
+        btCalcular.setOnClickListener {
+
+            val nota1 = Integer.parseInt(edTextnota1.text.toString())
+            val nota2 = Integer.parseInt(edTextNota2.text.toString())
+            val media = (nota1+nota2)/2
+            val faltas = Integer.parseInt(edTextFaltas.text.toString())
+            var resultado = textViewResultado
+
+            if (media >= 6 && faltas <= 10){
+                resultado.setText("Aluno Aprovado a media foi: \n" + media + "\n" + "faltas:"+faltas)
+            }else {
+                resultado.setText("Aluno Reprovado a media foi: \n" + media + "\n" + "faltas:" + faltas)
+            }
+        }
+
+
     }
 }
